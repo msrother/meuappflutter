f7b70c7660be7a02a7e1b84d7e5fba9f70188394
Marcelo Somenzi Rother <95832683+msrother@users.noreply.github.com>
2023-11-16 00:22:34 -0300

    Inclusão da função getFirstLetter

--- lib/controller/course_controller.dart
+++ lib/controller/course_controller.dart
@@ -4,6 +4,23 @@ import 'package:meuapp/model/course_repository.dart';
 class CourseController {
   CourseRepository repository = CourseRepository();
 
+// função para pegar a 1 letra da primeira palavra e a 1 letra da última palavra
+  String getFirstLetter(String? name) {
+    if (name == null || name.isEmpty) {
+      return '';
+    }
+
+    List<String> nameParts = name.split(' ');
+    if (nameParts.length < 2) {
+      return nameParts[0][0].toUpperCase();
+    }
+
+    String firstNameInitial = nameParts[0][0].toUpperCase();
+    String lastNameInitial = nameParts.last[0].toUpperCase();
+
+    return firstNameInitial + lastNameInitial;
+  }
+
   Future<List<CourseEntity>> getCourseList() async {
     List<CourseEntity> coursesList = [];
     coursesList = await repository.getAll();
@@ -13,6 +30,14 @@ class CourseController {
   deleteCourse(String id) async {
     try {
       await repository.deleteCourse(id);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  postNewCourse(CourseEntity courseEntity) async {
+    try {
+      await repository.postNewCourse(courseEntity);
     } catch (e) {
       rethrow;
     }
