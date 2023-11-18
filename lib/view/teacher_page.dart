3107d05be93a9e27a6d340cf553d191e6e7d9481
Marcelo Somenzi Rother <95832683+msrother@users.noreply.github.com>
2023-11-18 11:22:04 -0300

    Criação da tela professores

---
+++ lib/view/teacher_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TeacherPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Professores'),
+      ),
+      body: Center(
+        child: Text(
+          'Esta tela está em desenvolvimento!',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
